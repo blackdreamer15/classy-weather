4cfca4835f594d911454fa85e1f63ff3689d80d6
Jessy Baki <bakijessy@gmail.com>
2023-09-11 05:13:31 +0000

    Add a div.app and an h1 element to the JSX

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import React from "react";
 
 class App extends React.Component {
 
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Classy weather</h1>
+      </div>
+    );
+  }
 }
 
 
