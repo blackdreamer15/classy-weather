0ade0b501f9059a514e00e92832ec51df40618f4
Jessy Baki <bakijessy@gmail.com>
2023-09-11 05:20:35 +0000

    Turn the input into a controlled element

--- src/App.js
+++ src/App.js
@@ -14,7 +14,12 @@ class App extends React.Component {
         <h1>Classy weather</h1>
 
         <div>
-          <input type="text" placeholder="Search from location..." />
+          <input
+            type="text"
+            placeholder="Search from location..."
+            value={this.state.location}
+            onChange={e => this.setState.location(e.target.value)}
+          />
         </div>
 
         <button>Get weather</button>
